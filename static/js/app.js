26a008e8b74c4f38c94d0f37bf8e488f5e6ffa43
RahiqO <Rahiq.a.osman@gmail.com>
2023-05-28 13:56:33 -0400

    .

--- static/js/app.js
+++ static/js/app.js
@@ -1,37 +1,38 @@
-// const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-
-// d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then (data => console.log(data))
 
 
+ d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then (data => console.log(data))
 
-// import data from 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json' assert { type: 'json' };
-// console.log(data);
+ let sortedSearch = data.sort((a, b) => b - a);
 
-// fetch("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json")
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     for (let i = 0; i < data.length; i++) {
-//       console.log(data[i]);
-//     }
-//   })
+// Slice the first 10 objects for plotting
+slicedData = sortedSearch.slice(0, 10);
 
-//   var xhr = new XMLHttpRequest();
+// Reverse the array to accommodate Plotly's defaults
+// reversedData = slicedData.reverse();
 
-// // open a connection
-// xhr.open("GET", ".C:\Users\16475\Documents\GitLab resources\belly-button-challenge\static\js\samples.json", true);
+// Trace1 for the Greek Data
+let trace1 = {
+  x: slicedData.map(object => object.sample_values),
+  y: slicedData.map(object => object.otu_ids),
+  text: slicedData.map(object => object.otu_labels),
+  orientation: "h"
+};
 
-// // send the request
-// xhr.send();
+// Data array
+// `data` has already been defined, so we must choose a new name here:
+let traceData = [trace1];
 
-// // handle the response
-// xhr.onreadystatechange = function () {
-//   if (this.readyState == 4 && this.status == 200) {
-//     console.log(this.responseText);
-//   }
-// }
+// Apply a title to the layout
+let layout = {
+  title: "horizontal bar chart ",
+  margin: {
+    l: 100,
+    r: 100,
+    t: 100,
+    b: 100
+  }
+};
 
-fetch('https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json')
-   .then(response => response.text())
-   .then(text => console.log(text))
+// Render the plot to the div tag with id "plot"
+// Note that we use `traceData` here, not `data`
+Plotly.newPlot("plot", traceData, layout);
