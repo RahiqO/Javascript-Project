acea5fc750ce485f041cc7e0c5dfaa979fa7bc80
RahiqO <Rahiq.a.osman@gmail.com>
2023-05-28 15:06:58 -0400

    .

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,30 @@
 
 
- d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then (data => console.log(data))
+ d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then (data => {
+    
+ console.log(data);
 
- let sortedSearch = data.sort((a, b) => b - a);
+//  let sortedSearch = data.names.sort((a, b) => b - a);
+sample_values = data.samples[0].sample_values
+otu_ids = data.samples[0].otu_ids
+otu_labels = data.samples[0].otu_labels
+ slicedData = sample_values.slice(0, 10).reverse();
+ id_slicedData = otu_ids.slice(0, 10).reverse();
+ labels_slicedData = otu_labels.slice(0, 10).reverse();
 
 // Slice the first 10 objects for plotting
-slicedData = sortedSearch.slice(0, 10);
+// slicedData = sortedSearch.slice(0, 10);
 
 // Reverse the array to accommodate Plotly's defaults
 // reversedData = slicedData.reverse();
 
 // Trace1 for the Greek Data
 let trace1 = {
-  x: slicedData.map(object => object.sample_values),
-  y: slicedData.map(object => object.otu_ids),
-  text: slicedData.map(object => object.otu_labels),
-  orientation: "h"
+  x: slicedData,
+  y: id_slicedData.map(item => `OTU${item}`),
+  text: labels_slicedData,
+  orientation: "h",
+  type: "bar"
 };
 
 // Data array
@@ -35,4 +44,5 @@ let layout = {
 
 // Render the plot to the div tag with id "plot"
 // Note that we use `traceData` here, not `data`
-Plotly.newPlot("plot", traceData, layout);
+Plotly.newPlot("bar", traceData, layout);
+})
