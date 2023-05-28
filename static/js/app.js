4f598572905ad29cf19ffc9b96e3722a327f353a
RahiqO <Rahiq.a.osman@gmail.com>
2023-05-28 11:41:28 -0400

    first attempt

---
+++ static/js/app.js
@@ -0,0 +1,4 @@
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+let data_output = d3.json(url).then (data => console.log(data))
+
